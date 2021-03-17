83046a76a7426ca837b42be0a34edfe4b3eaa3d9
Quang Dang <quangdangfit@gmail.com>
2021-03-17 14:10:56 +0700

    add database package

---
+++ src/dbs/dbs.go
@@ -0,0 +1,40 @@
+package dbs
+
+import (
+	"fmt"
+
+	"github.com/jinzhu/gorm"
+
+	"github.com/quangdangfit/getjob/config"
+	"github.com/quangdangfit/getjob/pkg/logger"
+	"github.com/quangdangfit/getjob/src/interfaces"
+)
+
+type database struct {
+	db *gorm.DB
+}
+
+func NewDatabase() interfaces.IDatabase {
+	dbConfig := config.Config.Database
+	connectionPath := fmt.Sprintf("host=%s port=%d user=%s dbname=%s password=%s sslmode=%s",
+		dbConfig.Host, dbConfig.Port, dbConfig.User, dbConfig.Name, dbConfig.Password, dbConfig.SSLMode)
+
+	logger.Info(connectionPath)
+
+	db, err := gorm.Open("postgres", connectionPath)
+	if err != nil {
+		logger.Fatal("Cannot connect to database: ", err)
+	}
+
+	// Set up connection pool
+	db.DB().SetMaxIdleConns(20)
+	db.DB().SetMaxOpenConns(200)
+
+	return &database{
+		db: db,
+	}
+}
+
+func (d *database) GetInstance() *gorm.DB {
+	return d.db
+}
---
+++ src/interfaces/IDatabase.go
@@ -0,0 +1,7 @@
+package interfaces
+
+import "github.com/jinzhu/gorm"
+
+type IDatabase interface {
+	GetInstance() *gorm.DB
+}
