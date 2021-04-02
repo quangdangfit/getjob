e68f1a1da6314b680b8c2b91dc145ee31d3c42d8
Quang Dang <quangdangfit@gmail.com>
2021-04-02 14:50:37 +0700

    refactor router

--- app/router/api.go
+++ app/router/api.go
@@ -17,19 +17,22 @@ func RegisterAPI(r *gin.Engine, container *dig.Container) error {
 		companyAPI *api.CompanyAPI,
 	) error {
 		jwtMid := middleware.JWT(container)
-		//--------------------------------API-----------------------------------
+		//-------------------------------Public API-----------------------------
 		public := r.Group("/api/v1")
 		{
 			public.POST("/login", wrapper.Wrap(userAPI.Login))
 			public.POST("/register", wrapper.Wrap(userAPI.Register))
 		}
 
-		//--------------------------------API-----------------------------------
+		//-------------------------------Private API----------------------------
 		private := r.Group("/api/v1", jwtMid)
 		{
 			private.GET("/profile", jwtMid, wrapper.Wrap(userAPI.GetProfile))
 			private.POST("/companies", jwtMid, wrapper.Wrap(companyAPI.Create))
 		}
+
+		//-------------------------------Internal API----------------------------
+		//internal := r.Group("/api/v1/internal")
 
 		return nil
 	})
